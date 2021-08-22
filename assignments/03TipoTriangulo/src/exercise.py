e97d7327f23bf1e75d2116162a45f727005b78a6
Diana-Armenta <A01742155@itesm.mx>
2021-08-22 23:26:43 +0000

    ejercicios resueltos

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    x=int(input('Ingresa la medida del lado 1: '))
+    y=int(input('Ingresa la medida del lado 2: '))
+    z=int(input('Ingresa la medida del lado 3: '))
+    
+    if (x>0 and y>0 and z>0) and (x+y>z and x+z>y and z+y>x):
+        if x==y and y==z:
+            print('ES UN TRIANGULO EQUILATERO')
+        elif x==y or x==z or z==y:
+            print('ES UN TRIANGULO ISOSCELES')
+        else:
+            print('ES UN TRIANGULO ESCALENO')
+    else:
+        print('NO ES TRIANGULO')
 
 if __name__=='__main__':
     main()
